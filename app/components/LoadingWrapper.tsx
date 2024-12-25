2cd9fb03dd870aa00c3c507e3368b2e8b042fd71
engsiam <siam.softwareengineer@gmail.com>
2024-12-25 12:35:52 +0600

    loading image fix

--- app/components/LoadingWrapper.tsx
+++ app/components/LoadingWrapper.tsx
@@ -25,6 +25,7 @@ export default function LoadingWrapper({
             <img
               src="/assets/images/logo.png"
               alt="Loading Logo"
+              loading="lazy"
               className="w-20 h-20 animate-pulse mb-4"
             />
             {/* <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div> */}
